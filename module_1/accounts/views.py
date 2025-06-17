cec885ee0a2404be1725b36b4173e2f6c739deff
tvoiconfig <kolliceplay220@gmail.com>
2025-06-17 16:21:21 +0300

    Реализован функционал регистрации; Первый модуль выполнен

--- module_1/accounts/views.py
+++ module_1/accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from .forms import UserCreationForm
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = "register.html"
+    success_url = reverse_lazy("login")
+
+
