5ddb948e5649613e1da895fd48fac63c0fab76da
tvoiconfig <kolliceplay220@gmail.com>
2025-06-17 15:20:10 +0300

    Обновлена модель заявок, добавлен статус и причина отмены

--- module_1/applications/models.py
+++ module_1/applications/models.py
@@ -14,26 +14,39 @@ PAYMENT_CHOICES = [
     ("card", "Картой"),
 ]
 
+STATUS_CHOICES = [
+    ("new", "Новая заявка"),
+    ("completed", "Услуга оказана"),
+    ("canceled", "Услуга отменена"),
+]
+
 class ApplicationModel(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     address = models.CharField(max_length=100, verbose_name="Адрес")
     contacts = models.CharField(max_length=100, verbose_name="Контактные данные")
     service_type = models.CharField(
-        max_length=8,
-        choices=SERVICE_CHOICES,
-        default="main",
-        verbose_name="Вид услуги"
+        max_length = 8,
+        choices = SERVICE_CHOICES,
+        default = "main",
+        verbose_name = "Вид услуги"
     )
     payment_type = models.CharField(
-        max_length=5,
-        choices=PAYMENT_CHOICES,
-        default='card',
-        verbose_name="Тип оплаты"
+        max_length = 5,
+        choices = PAYMENT_CHOICES,
+        default = 'card',
+        verbose_name = "Тип оплаты"
     )
+    status = models.CharField(
+        max_length = 10,
+        choices = STATUS_CHOICES,
+        default="new",
+        verbose_name="Статус оказания услуги"
+    )
+    cancel_reason = models.TextField(blank=True, null=True, verbose_name="Причина отмены")
     
     class Meta:
         verbose_name = "Заявка"
         verbose_name_plural = "Заявки"
         
     def __str__(self):
-        return f"{self.author} {self.address}"
+        return f"Заявка №{self.id} от {self.author.full_name}"
